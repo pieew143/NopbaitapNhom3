9f6b76f52fe0381c64c487ba04772c26929235d7
Yến Nhi <119637727+pieew143@users.noreply.github.com>
2023-04-05 17:41:00 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ if __name__ == "__main__":
     n = int(input("Nhập n: "))
     
     print_primes(n)
-
+    a,b,c = map(int, input("Nhap 3 canh tam giac canh nhau 1 khoang trang : ").split())
+    canhtamgiac(a,b,c)
